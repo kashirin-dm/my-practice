f9d96a9f9974251ce67e9477b65914587d55458a
Dmitriy <dim.fly.design@gmail.com>
2019-01-21 06:26:53 +0600

    del dist and fix vue settings

--- vue-hh/vue.config.js
+++ vue-hh/vue.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   baseUrl: '',
-  publicPath: ''
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
 }
